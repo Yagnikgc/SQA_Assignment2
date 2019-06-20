7e98cc6bda051a5f415f164ad9c042cb9723654e
Yagnik Gordhanbhai Chauhan <Ychauhan5614@conestogac.on.ca>
2019-06-20 00:04:14 -0400

    project created

--- SQA_Assignment2/Program.cs
+++ SQA_Assignment2/Program.cs
@@ -10,12 +10,44 @@ namespace SQA_Assignment2
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            new Program().GO();
             Console.ReadKey();
+        }
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        private void GO()
+        {
+            int option = 0;
+            string optionStr = string.Empty;
+            do
+            {
+                Console.Write("\n\t1.Enter triangle dimensions\n\t2.Exit\n\nChoose Option: ");
+                optionStr = Console.ReadLine();
+                if (int.TryParse(optionStr, out option))
+                    switch(option)
+                    {
+                        case 1:
+                        TriangleSolver.SetSide1(TriangleSolver.Getvalue("1"));
+                        TriangleSolver.SetSide2(TriangleSolver.Getvalue("2"));
+                        TriangleSolver.SetSide3(TriangleSolver.Getvalue("3"));
+                        bool canForm = TriangleSolver.IsTriangle(TriangleSolver.GetSide1(), TriangleSolver.GetSide2(), TriangleSolver.GetSide3());
+                        if (canForm)
+                        {
+                            string type = TriangleSolver.Analyze(TriangleSolver.GetSide1(), TriangleSolver.GetSide2(), TriangleSolver.GetSide3());
+                            Console.WriteLine("\n\tIt is " + type + " Triangle");
+                        }
+                        else
+                            Console.WriteLine("\n\tValues Can't form Triangle");
+                            break;
+                        case 2:
+                            Console.WriteLine("\n\tThank you\n\tGood Bye");
+                            break;
+                        default:
+                            Console.WriteLine("\n\tInvalid Option\n\tplease enter again");
+                            break;
+                    }   
+                else
+                    Console.WriteLine("\n\tCharacters are not allowed\n\tplease enter again");
+            } while (option!=2);
         }
     }
 }
---
+++ SQA_Assignment2/TriangleSolver.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQA_Assignment2
+{
+    public static class TriangleSolver
+    {
+        static int side1;
+        static int side2;
+        static int side3;
+        static TriangleSolver()
+        {
+            side1 = side2 = side3 = 0;
+        }
+        public static int Getvalue(string param)
+        {
+            string value;
+            int val;
+            while(true)
+            {
+                Console.Write("\nEnter side "+param+": ");
+                value = Console.ReadLine();
+                if (int.TryParse(value, out val))
+                    if (val <= 0)
+                        Console.WriteLine("\n\tNegative value or Zero is not allowed\n\tplease enter again");
+                    else
+                        return val;
+                else
+                    Console.WriteLine("\n\tCharacters are not allowed\n\tplease enter again");
+            }
+        }
+        public static int GetSide1()
+        {
+            return side1;
+        }
+        public static int SetSide1(int val)
+        {
+            side1 = val;
+            return side1;
+        }
+        public static int GetSide2()
+        {
+            return side2;
+        }
+        public static int SetSide2(int val)
+        {
+            side2 = val;
+            return side2;
+        }
+        public static int GetSide3()
+        {
+            return side3;
+        }
+        public static int SetSide3(int val)
+        {
+            side3 = val;
+            return side3;
+        }
+        public static string Analyze(int side1, int side2, int side3)
+        {
+            if (side1 == side2 && side2 == side3)
+                return "Equilateral";
+            else if (side1 == side2 || side1 == side3 || side2 == side3)
+                return "Isosceles";
+            else
+                return "Scalene";
+        }
+        public static bool IsEquilateral(int side1, int side2, int side3)
+        {
+            if (side1 == side2 && side2 == side3)
+                return true;
+            else
+                return false;
+        }
+        public static bool IsIsosceles(int side1, int side2, int side3)
+        {
+            if (side1 == side2 || side1 == side3 || side2 == side3)
+                return true;
+            else
+                return false;
+        }
+        public static bool IsScalene(int side1, int side2, int side3)
+        {
+            if (side1 != side2 && side2 != side3 && side3 != side1)
+                return true;
+            else
+                return false;
+        }
+        public static bool IsTriangle(int side1, int side2, int side3)
+        {
+            // check condition 
+            if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1)
+                return false;
+            else
+                return true;
+        }
+    }
+}
