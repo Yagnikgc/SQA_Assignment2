2c45b85fda257b68e4b01ec266f22bceae737c1a
Yagnik Gordhanbhai Chauhan <Ychauhan5614@conestogac.on.ca>
2019-06-20 01:27:23 -0400

    completed console application

---
+++ TestTriangle/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestTriangle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestTriangle")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f423d23-c324-4b18-8698-2fbca93644ee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestTriangle/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SQA_Assignment2;
+
+namespace TestTriangle
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGetSide1()
+        {
+            int i = 4;
+            TriangleSolver.SetSide1(i);
+            int val = TriangleSolver.GetSide1();
+            Assert.AreEqual(val, i);
+        }
+    }
+}
